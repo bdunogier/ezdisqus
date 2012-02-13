fb180b19db3b89f33872b7e37b362587da5d9147
Jérôme Vieilledent <lolautruche@gmail.com>
2012-02-13 20:56:54 +0100

    Closed #17: Generate translation base file

---
+++ translations/untranslated/translation.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>ezdisqus/admin</name>
+    <message>
+        <source>Disqus comments</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Show comments from Disqus</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Loading comments...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ezdisqus/datatype</name>
+    <message>
+        <location filename="disquscommentstype.php" line="26"/>
+        <location filename="disquscommentstype.php" line="211"/>
+        <source>Disqus comments</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Comments activated by default</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Yes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
