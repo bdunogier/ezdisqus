fa80413b284fab9419cc873752799bfddb6608cd
Jérôme Vieilledent <lolautruche@gmail.com>
2012-02-13 21:01:03 +0100

    Closed #18 French translation

---
+++ translations/fre-FR/translation.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr_FR" sourcelanguage="en_GB">
+<context>
+    <name>ezdisqus/admin</name>
+    <message>
+        <source>Disqus comments</source>
+        <translation>Commentaires Disqus</translation>
+    </message>
+    <message>
+        <source>Show comments from Disqus</source>
+        <translation>Afficher les commentaires Disqus</translation>
+    </message>
+    <message>
+        <source>Loading comments...</source>
+        <translation>Chargement des commentaires</translation>
+    </message>
+</context>
+<context>
+    <name>ezdisqus/datatype</name>
+    <message>
+        <location filename="disquscommentstype.php" line="26"/>
+        <location filename="disquscommentstype.php" line="211"/>
+        <source>Disqus comments</source>
+        <translation>Commentaires Disqus</translation>
+    </message>
+    <message>
+        <source>Comments activated by default</source>
+        <translation>Commentaires activés par défaut</translation>
+    </message>
+    <message>
+        <source>Yes</source>
+        <translation>Oui</translation>
+    </message>
+    <message>
+        <source>No</source>
+        <translation>Non</translation>
+    </message>
+</context>
+</TS>
